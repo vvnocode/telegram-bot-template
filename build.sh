8364d86a79671288c9fb9558a181e32b5e04030e
ZhangWei <zhangwei@innovatech.net.cn>
2025-03-22 12:08:56 +0800

    fix

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ pip install pyinstaller
 # 使用PyInstaller打包
 pyinstaller --onefile \
     --name telegram-bot-template \
-    # --add-data "config/config.yaml.example:." \
     --hidden-import telegram \
     --hidden-import yaml \
     --paths src \
